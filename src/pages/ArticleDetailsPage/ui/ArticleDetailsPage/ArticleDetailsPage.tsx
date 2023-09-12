deba140ffbe5224d3ad229e96cb3ea8db2e60b68
vskill <vskill@mail.ru>
2023-09-12 11:04:49 +0300

    add feature flags concept

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -11,6 +11,7 @@ import { ArticleDetailsPageComments } from '../ArticleDetailsPageComments/Articl
 import { ArticleDetailsPageHeader } from '../ArticleDetailsPageHeader/ArticleDetailsPageHeader';
 import cls from './ArticleDetailsPage.module.scss';
 import { ArticleRating } from '@/features/articleRating';
+import { getFeatureFlag } from '@/shared/lib/features';
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -23,6 +24,8 @@ const reducers: ReducersList = {
 const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
     // const { t } = useTranslation('article');
     let { id } = useParams<{ id: string }>();
+    const isArticleRatingEnabled = getFeatureFlag('isArticleRatingEnabled');
+
     // TODO
     if (__PROJECT__ === 'storybook') {
         id = '1';
@@ -38,7 +41,7 @@ const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
                 <VStack gap="16" max>
                     <ArticleDetailsPageHeader />
                     <ArticleDetails id={id} />
-                    <ArticleRating articleId={id} />
+                    { isArticleRatingEnabled && <ArticleRating articleId={id} /> }
                     <ArticleRecommendationsList />
                     <ArticleDetailsPageComments id={id} />
                 </VStack>
---
+++ src/shared/lib/features/setGetFeatures.ts
@@ -0,0 +1,14 @@
+import { FeatureFlags } from "@/shared/types/featureFlags";
+
+let featureFlags: FeatureFlags;
+
+// features не меняются
+export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
+    if(newFeatureFlags) {
+        featureFlags = newFeatureFlags;
+    }
+}
+
+export function getFeatureFlag(flag: keyof FeatureFlags) {
+    return featureFlags[flag];
+}
